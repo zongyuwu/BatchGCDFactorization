fa2a01f7299d26ef916bb81eb74f8cdecbad5bb0
zongyuwu <w.zongyu@gmail.com>
2015-09-30 17:32:49 +0800

    initial

---
+++ batchgcdfactorization_api.rb
@@ -0,0 +1,22 @@
+#!/usr/bin/env ruby
+
+class Batchgcd
+  def initialize(n=nil)
+    @N = n
+  end
+
+  def update
+  end
+
+  def update_from_file
+  end
+
+  def exploit
+  end
+
+private
+end
+
+test_n_dir = "./test/modulus/test_case"
+test_pem_dir = "./test/pem/"
+
